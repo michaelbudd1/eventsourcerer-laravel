3fe575a2f82705f86e9cd7ac0c91732f6a0eee12
michaelbudd <michaelbudd6@gmail.com>
2025-07-19 06:19:09 +0100

    wip

--- src/Console/Commands/FetchEvents.php
+++ src/Console/Commands/FetchEvents.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Eventsourcerer\EventSourcererLaravel\Console\Commands;
 
 use Eventsourcerer\EventSourcererLaravel\EventHandler;
--- src/config/eventsourcerer.php
+++ src/config/eventsourcerer.php
@@ -2,7 +2,9 @@
 
 return [
     'applicationId' => env('EVENT_SOURCERER_APPLICATION_ID'),
-    'cache'         => storage_path('eventsourcerer'),
+    'cache'         => [
+        'path' => storage_path('eventsourcerer')
+    ],
     'host'          => env('EVENT_SOURCERER_SERVER_HOST'),
     'port'          => env('EVENT_SOURCERER_SERVER_PORT'),
     'url'           => env('EVENT_SOURCERER_SERVER_URL'),
