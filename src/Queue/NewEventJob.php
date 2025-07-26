0dfcf42762d46c322257f36dc05151fd077c6283
michaelbudd <michaelbudd6@gmail.com>
2025-07-26 07:27:58 +0100

    wip

--- src/Queue/NewEventJob.php
+++ src/Queue/NewEventJob.php
@@ -5,9 +5,10 @@ declare(strict_types=1);
 namespace Eventsourcerer\EventSourcererLaravel\Queue;
 
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class NewEventJob implements ShouldQueue
+class NewEventJob implements ShouldQueue, ShouldBeUnique
 {
     use Queueable;
 
@@ -16,5 +17,11 @@ class NewEventJob implements ShouldQueue
     public function handle(): void
     {
         dump($this->event);
+    }
+
+    public function uniqueId(): string
+    {
+        dd($this->event);
+        return $this->event['streamId'];
     }
 }
