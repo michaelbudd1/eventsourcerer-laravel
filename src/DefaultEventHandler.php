e0d1f4e1680858f7a0821c5ba325cdd3620d88b3
michaelbudd <michaelbudd6@gmail.com>
2025-07-27 07:40:48 +0100

    wip

--- src/DefaultEventHandler.php
+++ src/DefaultEventHandler.php
@@ -11,7 +11,7 @@ final readonly class DefaultEventHandler implements EventHandler
     public function handle(): callable
     {
         return static function (array $event): void {
-            $job = (new NewEventJob($event))->onConnection('eventsourcerer');
+            $job = (new NewEventJob($event))->onConnection('sync');
 
             dispatch($job);
         };
